66f26704e8b1729bb5bbc982d472315a95e7eab1
DorinBelascu <Dorin.Belascu@yahoo.com>
2020-11-28 13:15:03 +0200

    =Grouped tests together

--- tests/index.test.js
+++ tests/index.test.js
@@ -1,15 +1,18 @@
 const index = require('../index');
-test('absolute - should return a positive number  if input is positive', () => {
-    const result = index.absolute(1);
-    expect(result).toBe(1);
-});
 
-test('absolute - should return a positive number  if input is negative', () => {
-    const result = index.absolute(-1);
-    expect(result).toBe(1);
-});
-
-test('absolute - should return zero number if input is zero', () => {
-    const result = index.absolute(0);
-    expect(result).toBe(0);
+describe('absolute', () =>{ // Group the tests together 
+    it('should return a positive number  if input is positive', () => {
+        const result = index.absolute(1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return a positive number  if input is negative', () => {
+        const result = index.absolute(-1);
+        expect(result).toBe(1);
+    });
+    
+    it('should return zero number if input is zero', () => {
+        const result = index.absolute(0);
+        expect(result).toBe(0);
+    });
 });
