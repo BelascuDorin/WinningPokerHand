17ba2d43fb74977ac4debf3e199ca925c264fe53
DorinBelascu <Dorin.Belascu@yahoo.com>
2020-11-28 14:02:47 +0200

    =added string and array unit testing examples

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 module.exports.absolute = function(number){
     if(number >= 0) return number;
     return -number;
-}
+};
 
+module.exports.greet = function(name){
+    return 'Welcome ' + name;
+};
+
+module.exports.getCurrencies = function(){
+    return ['USD', 'AUD', 'EUR'];
+};
