505fc77e923533d4a5d94df03f50bf0fbf22d2f0
DorinBelascu <Dorin.Belascu@yahoo.com>
2020-11-28 13:27:55 +0200

    =refactoringn of the absolute function

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 module.exports.absolute = function(number){
-    if(number > 0) return number;
-    if(number < 0) return -number;
-    return 0;
+    if(number >= 0) return number;
+    return -number;
 }
+
