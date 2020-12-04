c69864a6db328eba660b6ccb1ee6187c8c24a74d
DorinBelascu <Dorin.Belascu@yahoo.com>
2020-12-04 17:27:36 +0200

    =Added enums, test case it should not work with duplicate cards

--- tests/winningHand.test.js
+++ tests/winningHand.test.js
@@ -1,30 +1,49 @@
-const winningHand = require('../winningHand');
+const Enum = require('enum')
 
-describe('evaluateHandOfCards', () =>{ // Group the tests together 
-    function getDefaultHandOfCards(){
-        return {nrOfCards: 7};
+const winningHand = require('../winningHand');
+const CardTypes = new Enum(['CLUB', 'DIAMOND', 'SPADE', 'HEARTH']);
+const CardNumbers = new Enum(['2', '3', '4', '5', '6', '7', '8', '9', '10', 'J', 'Q', 'K', 'A']);
+
+describe('evaluateHandOfCards', () => { // Group the tests together 
+    function getValidHandOfCards() {
+
+        return {
+            nrOfCards: 7,
+            cards: [
+                { "number": CardNumbers.get('2').value, "type": CardTypes.get('CLUB').value },
+                { "number": CardNumbers.get('3').value, "type": CardTypes.get('CLUB').value },
+                { "number": CardNumbers.get('4').value, "type": CardTypes.get('CLUB').value },
+                { "number": CardNumbers.get('A').value, "type": CardTypes.get('CLUB').value },
+                { "number": CardNumbers.get('2').value, "type": CardTypes.get('SPADE').value },
+                { "number": CardNumbers.get('4').value, "type": CardTypes.get('DIAMOND').value },
+            ]
+        };
     }
 
     it('should work only with a 7 cards hand', () => {
         expect(() => {
-            winningHand.evaluate7CardsPokerHand(getDefaultHandOfCards())
-            }).not.toThrow();
+            winningHand.evaluate7CardsPokerHand(getValidHandOfCards())
+        }).not.toThrow();
 
-        h8 = getDefaultHandOfCards();
+        h8 = getValidHandOfCards();
         h8.nrOfCards = 8;
         expect(() => {
             winningHand.evaluate7CardsPokerHand(h8)
-            }).toThrow();            
+        }).toThrow();
     });
 
-    it('should be an evaluation scale returned', () => {
-        const evaluationScale = winningHand.evaluate7CardsPokerHand(getDefaultHandOfCards());
+    it('should have an evaluation scale returned', () => {
+        const evaluationScale = winningHand.evaluate7CardsPokerHand(getValidHandOfCards());
         expect(evaluationScale).not.toBeNull();
     });
 
-    it('should be an evaluation scale returned', () => {
-        const evaluationScale = winningHand.evaluate7CardsPokerHand(getDefaultHandOfCards());
-        expect(evaluationScale).not.toBeNull();
+    it('should not work with a hands of duplicated cards', () => {
+        const duplicateCardInHand = getValidHandOfCards();
+        duplicateCardInHand.cards[0] = duplicateCardInHand.cards[1];
+        console.log(duplicateCardInHand);
+        expect(() => {
+            winningHand.evaluate7CardsPokerHand(duplicateCardInHand)
+        }).toThrow();
     });
 });
 
--- winningHand.js
+++ winningHand.js
@@ -1,6 +1,19 @@
+const findDuplicates = require('array-find-duplicates');
+
+function thereAreDuplicatedCards(cards){
+    const result = findDuplicates(cards, (a, b) => a.number === b.number && a.type === b.type);
+    console.log(result.length)    ;
+    if(result.length === 0) return 0;
+    else return 1;
+}
+
 module.exports.evaluate7CardsPokerHand = function(pokerHand){
     if(pokerHand.nrOfCards !== 7 ){
         throw new Error('The poker hand should have only 7 cards.');
+    }
+
+    if(thereAreDuplicatedCards(pokerHand.cards) === 1){
+        throw new Error('Invalid Hand. There are at least 2 duplicated cards.');
     }
 
     return 1;
